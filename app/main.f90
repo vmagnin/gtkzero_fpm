be3d1062603904a6fb31031dead0330b39ce4dd0
vmagnin <vincent.magnin@libertysurf.fr>
2022-04-13 20:33:03 +0200

    Replaced GPL by MIT license

--- app/main.f90
+++ app/main.f90
@@ -1,25 +1,5 @@
-! Copyright (C) 2011
-! Free Software Foundation, Inc.
-!
-! This file is part of the gtk-fortran GTK / Fortran Interface library.
-!
-! This is free software; you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation; either version 3, or (at your option)
-! any later version.
-!
-! This software is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-!
-! Under Section 7 of GPL version 3, you are granted additional
-! permissions described in the GCC Runtime Library Exception, version
-! 3.1, as published by the Free Software Foundation.
-! You should have received a copy of the GNU General Public License along with
-! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
-! If not, see <http://www.gnu.org/licenses/>.
 !-------------------------------------------------------------------------------
+! MIT license
 ! Vincent MAGNIN, 2020-02-18
 ! Last modified: 2020-05-07
 ! This example just creates a GTK application with an empty window.
--- src/handlers.f90
+++ src/handlers.f90
@@ -1,4 +1,6 @@
 !*************************************
+! MIT license
+! Vincent MAGNIN
 ! User defined event handlers go here
 !*************************************
 module handlers
